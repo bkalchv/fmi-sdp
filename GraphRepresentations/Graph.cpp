ef9e4280172f9208f2aa64f2d3732b57454106ff
Bogdan Kalchev <bogdan.kalchev@mentormate.com>
2021-01-14 23:14:25 +0200

    Open file once only, readVertices and addEdges -> readVerticesAddEdges

--- GraphRepresentations/Graph.cpp
+++ GraphRepresentations/Graph.cpp
@@ -43,12 +43,79 @@ void addEdge(size_t from, size_t to)
 	}
 }
 
-set<size_t> readVertices(ifstream& inMyfile)
+// DFS hasCycleRec() for a directed graph
+//bool hasCycleRec(size_t vertexIndex, vector<bool>& isVisited, vector<bool>& isCurrent)
+//{
+//	if (isCurrent[vertexIndex])
+//	{
+//		return true;
+//	}
+//
+//	isCurrent[vertexIndex] = true;
+//
+//	for (size_t neighbour : adjacencyLists[vertexIndex])
+//	{
+//		if (!isVisited[neighbour])
+//		{
+//			if (hasCycleRec(neighbour, isVisited, isCurrent))
+//				return true;
+//		}
+//	}
+//
+//	isVisited[vertexIndex] = true;
+//	return false;
+//}
+//
+//// DFS hasCycle() for a directed graph
+//bool hasCycle()
+//{
+//	vector<bool> isVisited;
+//	vector<bool> isCurrent; // If it is an directed Graph (We have to check where we're coming from)
+//	isVisited.resize(adjacencyLists.size(), false);
+//	isCurrent.resize(adjacencyLists.size(), false);
+//	return hasCycleRec(0, isVisited, isCurrent);
+//}
+
+//void DFSRemoveVisited(size_t from, std::set<size_t>& vertices)
+//{
+//	vertices.erase(from);
+//
+//	for (size_t neighbour : adjacencyLists[from])
+//	{
+//		if (vertices.find(neighbour) != vertices.end())
+//		{
+//			DFSRemoveVisited(neighbour, vertices);
+//		}
+//	}
+//}
+
+//size_t countComponents()
+//{
+//	size_t count = 0;
+//	std::set<size_t> vertices;
+//
+//	for (size_t i = 0; i < adjacencyLists.size(); ++i)
+//	{
+//		vertices.insert(i);
+//	}
+//
+//	while (!vertices.empty())
+//	{
+//		++count;
+//		size_t currentVertex = *vertices.begin();
+//		DFSRemoveVisited(currentVertex, vertices);
+//	}
+//
+//	return count;
+//}
+
+void readVerticesAddEdges(std::string filename, std::set<size_t>& vertices)
 {
-	std::set<size_t> vertices;
+	ifstream inMyfile;
+	inMyfile.open(filename.c_str());
 	std::string line;
 
-	if (inMyfile.is_open())
+	if (inMyfile)
 	{
 		int number1;
 		int number2;
@@ -66,6 +133,30 @@ set<size_t> readVertices(ifstream& inMyfile)
 				//cout << "line: " << openingBracket << number1 << " " << number2 << closingBracket << endl;
 				vertices.insert(number1);
 				vertices.insert(number2);
+
+				if (adjacencyLists.find(number1) == adjacencyLists.end())
+				{
+					adjacencyLists[number1];
+				}
+
+				if (adjacencyLists.find(number2) == adjacencyLists.end())
+				{
+					adjacencyLists[number2];
+				}
+
+				if (!hasEdge(number1, number2))
+				{
+					addEdge(number1, number2);
+					if (adjacencyLists[number1].empty())
+					{
+						cout << "Faulty add!" << endl;
+					}
+					else
+					{
+						cout << "Edge between " << number1 << " and " << number2 << " added successfully!" << endl;
+					}
+
+				}
 			}
 
 			if (iss2 >> openingBracket >> number1 >> closingBracket
@@ -74,50 +165,19 @@ set<size_t> readVertices(ifstream& inMyfile)
 			{
 				//cout << "line: " << openingBracket << number1 << closingBracket << endl;
 				vertices.insert(number1);
-			}
-		}
-	}
 
-	inMyfile.close();
-	return vertices;
-}
-
-void addEdgesFromFile(ifstream& inMyfile)
-{
-	std::string line;
-
-	if (inMyfile.is_open())
-	{
-		int number1;
-		int number2;
-		char openingBracket;
-		char closingBracket;
-		while (getline(inMyfile, line))
-		{
-			std::istringstream iss1(line);
-			std::istringstream iss2(line);
-
-			if (iss1 >> openingBracket >> number1 >> number2 >> closingBracket
-				&& openingBracket == '['
-				&& closingBracket == ']')
-			{
-				cout << "line: " << openingBracket << number1 << " " << number2 << closingBracket << endl;
-				if (!hasEdge(number1, number2))
+				if (adjacencyLists.find(number1) == adjacencyLists.end())
 				{
-					addEdge(number1, number2);
-					if (adjacencyLists[number1].empty())
-					{
-						cout << "Faulty add!" << endl;
-					}
-					else 
-					{
-						cout << "Edge between " << number1 << " and " << number2 << " added successfully!" << endl;
-					}
-
+					adjacencyLists[number1];
 				}
 			}
 		}
 	}
+	else
+	{
+		cout << "Couldn't open file." << endl;
+	}
+
 	inMyfile.close();
 }
 
@@ -146,33 +206,26 @@ void printAdjacencyLists()
 }
 
 int main()
-{	
-	std::ifstream inMyfile ("Text.txt");
-	std::ifstream inMyfile1("Text.txt");
-	set<size_t> vertices = readVertices(inMyfile);
+{
+	std::set<size_t> vertices;
+	readVerticesAddEdges("Text.txt", vertices);
 
-	cout << "vertices: " ;
+	cout << "vertices: ";
 	for (const size_t& vertex : vertices)
 	{
 		cout << vertex << " ";
 	}
 	cout << endl;
 
-	for (size_t vertex : vertices)
-	{
-		adjacencyLists[vertex];
-	}
-
 	for (const size_t& vertex : vertices)
 	{
 		if (adjacencyLists.find(vertex) == adjacencyLists.end())
 		{
-			cout << vertex << "not in unordered_map! ";
+			cout << vertex << "not in ordered_map! ";
 			cout << "Unexpected behavior!" << endl;
 		}
 	}
 
-	addEdgesFromFile(inMyfile1);
 	printAdjacencyLists();
 
 	return 0;
